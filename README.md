39501f6f910c8ef485f489a63cf11bcefd9cfdd7
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-07-22 11:55:36 +0200

    first version - dev

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# spectangular-training
-Application for training writing e2e tests
+# Spectangular training
+
+
+This repository contains an application which is used for trainings of writing e2e tests with Spectangular.
+
+# Spectangular
+
+Spectangular is a library which helps with writing e2e tests for frontend applications and websites.
+See [https://github.com/schubergphilis/spectangular](https://github.com/schubergphilis/spectangular) 
+
+## Installation
+
+The following steps will install / update you development environment. After installing the NPM & Bower packages, it will start the development server and start Grunt watch with LiveReload
+
+- `npm install -d`
+- `grunt build`
+- `grunt start`
+- `http://localhost:1985`
+
+
--- bin/dev.js
+++ bin/dev.js
@@ -1,33 +1,33 @@
 /* jshint node: true */
 'use strict';
 var express = require('express'),
-    proxy = require('express-http-proxy'),
-    Stubby = require('stubby').Stubby,
-    yaml = require('read-yaml'),
-    dev = express(),
-    //proxy
-    apiProxy = proxy('127.0.0.1:1986', {
-        forwardPath: function(req) {
-            return require('url').parse(req.url).path;
-        }
-    }),
-    server, mockService;
-    //start mock service
-    mockService = new Stubby();
-    mockService.start({
-        stubs: 1986,
-        mute: false,
-        data: yaml.sync('./spec/stub/ipl.yml')
+  proxy = require('express-http-proxy'),
+  Stubby = require('stubby').Stubby,
+  yaml = require('read-yaml'),
+  dev = express(),
+//proxy
+  apiProxy = proxy('127.0.0.1:1986', {
+    forwardPath: function (req) {
+      return require('url').parse(req.url).path;
+    }
+  }),
+  server, mockService;
+//start mock service
+mockService = new Stubby();
+mockService.start({
+  stubs: 1986,
+  mute: false,
+  data: yaml.sync('./spec/stub/ipl.yml')
 
-    });
-    //index HTML
-    dev.use(express.static('./'));
-    //API proxy
-    dev.use('/api/v2/', apiProxy);
-    //start dev app
-    server = dev.listen(1985, function () {
-        var host = server.address().address,
-            port = server.address().port;
-        console.log('IPL app listening at http://%s:%s', host, port);
-    });
+});
+//index HTML
+dev.use(express.static('./'));
+//API proxy
+dev.use('/api/v2/', apiProxy);
+//start dev app
+server = dev.listen(1985, function () {
+  var host = server.address().address,
+    port = server.address().port;
+  console.log('IPL app listening at http://%s:%s', host, port);
+});
     
