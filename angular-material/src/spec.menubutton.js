39a9926b37ff7bffc0409c4a0e6d93a1aee0b796
Martijn Gijsberti Hodenpijl <mgijsbertihodenpijl@schubergphilis.com>
2015-08-14 12:20:47 +0200

    exercise menubutton

---
+++ angular-material/src/spec.menubutton.js
@@ -0,0 +1,22 @@
+/* globals browser, describe, it, element, by, expect, beforeEach */
+import Spectangular from '../node_modules/spectangular/dist/spectangular.js';
+import * as SpectangularMdLibrary from '../node_modules/spectangular/dist/libraries/md/md.js';
+
+Spectangular.baseUrl = 'https://material.angularjs.org/latest';
+Spectangular.library = SpectangularMdLibrary;
+
+describe('Demo of menu buttons', function () {
+
+    /**
+     * Loads the page https://material.angularjs.org/latest/#/demo/material.components.menu and waits for the element
+     * with css selector .demo-toolbar.
+     */
+    beforeEach(function () {
+        Spectangular.loadPage('/#/demo/material.components.menu', '.demo-toolbar');
+    });
+
+    it('should open menu in Basic Usage and click on redial', function () {
+        //Exercise 7: Select the menu button in the Basic Usage section, click on 'redial' options and click
+        // on the button in the dialog with text 'That was easy'
+    });
+});
